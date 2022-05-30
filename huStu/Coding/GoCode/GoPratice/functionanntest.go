4c9c56298455ff7e3a37d75ba6cb155dc423d011
hchcfly <hchc32@163.com>
2022-05-30 20:47:51 +0800

    go

---
+++ huStu/Coding/GoCode/GoPratice/functionanntest.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+
+
+func main() {
+	max := func(a int, b int) int {
+		if a > b {
+			return a
+		} else {
+			return b
+		}
+	}(1,2)
+	// r := max(1,2)
+	fmt.Printf("max: %v\n", max)
+}
---
+++ huStu/Coding/GoCode/GoPratice/functionclosuretest.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func add() func(y int)int {
+	var x int
+	return func(y int) int {
+		x+=y
+		return x
+	}
+}
+
+
+func cal(base int) (func (int)int,func (int)int) {
+	add := func(a int) int {
+		base += a
+		return base
+	}
+
+	sub := func(a int) int {
+		base -= a
+		return base
+	}
+
+	return add,sub
+}
+
+func main() {
+	add,sub := cal(100)
+	r := add(100)
+	fmt.Printf("r: %v\n", r)
+	r = sub(20)
+	fmt.Printf("r: %v\n", r)
+	// f := add()
+	// r := f(10)
+	// fmt.Printf("r: %v\n", r)
+	// r = f(20)
+	// fmt.Printf("r: %v\n", r)
+	// r = f(30)
+	// fmt.Printf("r: %v\n", r)
+	// fmt.Println("---------------------")
+
+	// f1 := add()
+	// r = f1(100)
+	// fmt.Printf("r: %v\n", r)
+	// r = f1(100)
+	// fmt.Printf("r: %v\n", r)
+
+}
