4fd51ffb40233366361c8eb5686c596b9e8003bc
hchcfly <hchc32@163.com>
2022-06-10 22:05:59 +0800

    go

---
+++ huStu/Coding/AlgorithmCode/剑指/53_在排序数组中查找数字2.cc
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+        //--------------------------1
+        // int len = nums.size();
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(i!=nums[i]) return i;
+        // }
+        // return nums[len-1]+1;
+
+        //--------------------------2二分0(logN)
+        int left=0;
+        int right=nums.size()-1;
+        while(left < right)
+        {
+            int mid = (right-left)/2+left;
+            if(nums[mid] == mid)
+            {
+                left = mid+1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+        cout << right << endl;
+        return right==nums[nums.size()-1]?right+1:right;
+    }
+};
