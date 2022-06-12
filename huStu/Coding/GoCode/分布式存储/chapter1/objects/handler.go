6e7e387db4c23e4d83a0d0479520ade9a838e784
hchcfly <hchc32@163.com>
2022-06-12 11:58:12 +0800

    go

--- huStu/Coding/GoCode/分布式存储/chapter1/objects/handler.go
+++ huStu/Coding/GoCode/分布式存储/chapter1/objects/handler.go
@@ -1,14 +1,16 @@
 package objects
 
 import "net/http"
+import "fmt"
 
 func Handler(w http.ResponseWriter , r * http.Request){
 	m := r.Method
+	fmt.Println("method = ",m)
 	if m == http.MethodPut {
-
+		put(w,r)
 	}
 	if m == http.MethodGet {
-
+		get(w,r)
 	}
 	w.WriteHeader(http.StatusMethodNotAllowed)
 }
--- huStu/Coding/GoCode/分布式存储/chapter1/server.go
+++ huStu/Coding/GoCode/分布式存储/chapter1/server.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"log"
 	"os"
+	"./objects"
 )
 
 
