22e7b714b7db5c66f4c339cd5f98d2d0ad7467e8
hchcfly <hchc32@163.com>
2022-06-18 16:17:44 +0800

    go

--- huStu/Coding/GoCode/分布式存储/chapter3/apiServer/objects/get.go
+++ huStu/Coding/GoCode/分布式存储/chapter3/apiServer/objects/get.go
@@ -10,7 +10,34 @@ import(
 func get(w http.ResponseWriter,r *http.Request) {
 	//  从URL中解析<object_name>部分赋值给object
 	object := strings.Split(r.URL.EscapedPath(),"/")[2]
-	stream , e := getStream(object)
+	//  查询版本号数组
+	versionId := r.URL.Query()["version"]
+	version := 0
+	var e error
+	if len(versionId) != 0{
+		//  获取对象对应版本号
+		version,e = strconv.Atoi(versionId[0])
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+	//  通过名称和版本号查询元数据
+	meta,e := es.GetMetadata(name,version)
+	if e != nil {
+		log.Println(e)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if meta.Hash == "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	//  获取对象的散列值
+	object := url.PathEscape(meta.Hash)
+	//  获取对应stram采用hash value
+	stream,e := getStream(object)
 	if e != nil {
 		log.Println(e)
 		w.WriteHeader(http.StatusNotFound)
--- huStu/Coding/GoCode/分布式存储/chapter3/apiServer/objects/put.go
+++ huStu/Coding/GoCode/分布式存储/chapter3/apiServer/objects/put.go
@@ -9,13 +9,35 @@ import(
 将HTTP请求转发给数据服务
 */
 func put(w http.ResponseWriter, r *http.Request) {
-	//  从URL中解析<object_name>部分赋值给object
-	object := strings.Split(r.URL.EscapedPath(),"/")[2]
-	c , e := storeObject(r.Body,object)
+	//  从http请求头部获取对象的hash值
+	hash := utils.GetHashFromHeader(r.Header)
+	if hash == "" {
+		log.Println("missing object hash in digest header")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	//  根据对象的hash值存储对象
+	c , e := storeObject(r.Body,url.PathEscape(hash))
 	if e != nil {
 		log.Println(e)
+		w.WriteHeader(c)
+		return
 	}
-	//  错误代码写入HTTP响应中
-	w.WriteHeader(c)
+	if c != http.StatusOK {
+		w.WriteHeader(c)
+		return
+	}
+	//  从URL中获取对象名称
+	name := strings.Split(r.URL.EscapedPath(),"/")[2]
+	//  从URL中获取对象的size
+	size := utils.GetSizeFromHeader(r.Header)
+	//  向es中添加新版本对象的metadata
+	e = es.AddVersion(name,hash,size)
+	if e != nil {
+		log.Println(e)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
+
+
 
