688ad65e924733b49c81ea6f1a91a789d1084f9b
hchcfly <hchc32@163.com>
2022-07-13 08:04:36 +0800

    go

---
+++ huStu/Coding/AlgorithmCode/剑指/16_数值的整数次方.cc
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) 
+    {
+        //快速幂
+        if(x==0) return 0;
+        long b = n;
+        double res = 1.0;
+        if(b < 0)
+        {
+            x = 1/x;
+            b = -b;
+        }
+        while(b > 0)
+        {
+            //奇数
+            if((b&1)==1) res*=x;
+            x *= x;
+            b>>=1;
+        }
+        return res;
+
+
+    }
+};
